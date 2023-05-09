8e43be2b023e6df5b570e8def433e6286942e5e2
Nguyenhv90 <nguyenhv236@gmail.com>
2023-05-09 16:07:44 +0700

    First Commit for branch

---
+++ supportpotal/src/test/java/com/hvn/supportpotal/SupportpotalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hvn.supportpotal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SupportpotalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
